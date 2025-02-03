dd22de2557438b80f073ce1e5da59d9a7f997417
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-03 17:09:41 +0100

    1

--- github/start.sh
+++ github/start.sh
@@ -7,17 +7,15 @@ die() {
     exit 1
 }
 
-[ "$GITHUB_REPO" ] || die "missing repo url"
-[ "$GITHUB_TOKEN" ] || die "missing token"
-[ "$VM_PORT" ] || die "missing VM port"
-[ "$GITHUB_ORG" ] || die "missing organisation"
+[ "$GITHUB_REPO"   ] || die "missing repo url"
+[ "$GITHUB_TOKEN"  ] || die "missing token"
+[ "$GITHUB_ORG"    ] || die "missing organisation"
+[ "$GITHUB_LABELS" ] || die "missing GITHUB_LABELS"
+[ "$VM_PORT"       ] || die "missing VM port"
 
 cd /home/github_runner
 
-curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/${GITHUB_ORG}/actions/runners/registration-token
-
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/${GITHUB_ORG}/actions/runners/registration-token | jq .token --raw-output)
-echo "REG_TOKEN=$REG_TOKEN"
 
-./config.sh --unattended --url "$GITHUB_REPO" --token "$REG_TOKEN"
+./config.sh --unattended --url "$GITHUB_REPO" --token "$REG_TOKEN" --labels "$GITHUB_LABELS"
 ./run.sh

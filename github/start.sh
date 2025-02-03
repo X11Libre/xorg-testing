a8751c5380ee17fc3e7ced4e6e5ad58767ffa569
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-03 16:43:47 +0100

    1
    1

--- github/start.sh
+++ github/start.sh
@@ -10,8 +10,14 @@ die() {
 [ "$GITHUB_REPO" ] || die "missing repo url"
 [ "$GITHUB_TOKEN" ] || die "missing token"
 [ "$VM_PORT" ] || die "missing VM port"
+[ "$GITHUB_ORG" ] || die "missing organisation"
 
 cd /home/github_runner
 
-./config.sh --unattended --url "$GITHUB_REPO" --token "$GITHUB_TOKEN"
+curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/${GITHUB_ORG}/actions/runners/registration-token
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/orgs/${GITHUB_ORG}/actions/runners/registration-token | jq .token --raw-output)
+echo "REG_TOKEN=$REG_TOKEN"
+
+./config.sh --unattended --url "$GITHUB_REPO" --token "$REG_TOKEN"
 ./run.sh

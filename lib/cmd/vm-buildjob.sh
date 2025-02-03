a1b599acd7e45e41adeec76f6297468beb24abfb
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-03 11:04:27 +0100

    vm: scripts for booting VM and running build job
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ lib/cmd/vm-buildjob.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+SCRIPT_ROOT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+. $SCRIPT_ROOT_DIR/lib/util.sh
+
+bn=`basename $0`
+bn="${bn/vm-buildjob-/}"
+
+. $SCRIPT_ROOT_DIR/etc/vm/$bn.cf || die "cant find config for $bn"
+. $SCRIPT_ROOT_DIR/lib/vm.sh
+
+boot_and_wait_for_vm
+vm_build_testing_jail
